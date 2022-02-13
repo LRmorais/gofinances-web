e4ccebb83ed40438be17681ac876ef36e4dedb15
Lucas Morais <lucasmorais@clubegazetadopovo.com.br>
2022-02-13 19:37:23 -0300

    feat: global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,6 +4,7 @@ export const GlobalStyles = createGlobalStyle`
 :root{
   --red: #e52e4d;
   --blue: #5429cc;
+  --green: #33cc95;
 
   --blue-light: #6933ff;
 
@@ -32,7 +33,7 @@ export const GlobalStyles = createGlobalStyle`
   }
 
   body {
-    background: var()(--background);
+    background: var(--background);
     --webkit-font-smoothing: antialiased;
   }
 
